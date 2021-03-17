4fe5b4f247d55a8d1faaba87b89bc181ffc51e82
AleksandarMoraca <80708939+AleksandarMoraca@users.noreply.github.com>
2021-03-17 16:43:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Home </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Aleksandar Moraca">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+
+<nav>
+    <input type="checkbox" id="check">
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+    <label class="logo">
+        <b class="moraca">MoracaA</b>
+    </label>
+    <ul>
+        <li><a href="index.php" class="active"> Home </a></li>
+        <li><a href="Jobs.php"> Jobs </a></li>
+        <li><a href="Companies.php"> Contact Me </a></li>
+    </ul>
+    </nav>
+
+
+
+
+<footer class="foot">
+    &copy;by Moraca Aleksandar
+</footer>
+</body>
+</html>
+
+
+
+<?php
+?>
